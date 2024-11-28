1d369dfa1158980a8ebaf99b810e2b801484c758
Rabiul Hasan Rafee <rhr277@gmail.com>
2024-11-29 02:58:08 +0600

    VERCEL.JSON ADDED

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const todoHandler = require("./routeHandler/todoHandler");
@@ -8,31 +8,32 @@ const connectDB = require("./DB/connectDB");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-    {
+app.use(
+    cors({
         origin: ["http://localhost:5173", "https://todowebappx.netlify.app"],
-        credentials: true
-    }
-));
+        credentials: true,
+    })
+);
+
+// Connect to the database
+connectDB();
 
 // Routes
 app.use("/user", userHandler);
 app.use("/todo", todoHandler);
 
-
+// Error handler
 const errorHandler = (err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
     }
     res.status(500).json({ error: err });
-}
+};
 app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-    connectDB();
-});
+// Export the app for Vercel
+module.exports = app;
