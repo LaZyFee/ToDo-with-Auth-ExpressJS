43f0cd743048ebbb4d579b37eb5afc632b9f3be3
Rabiul Hasan Rafee <rhr277@gmail.com>
2024-11-29 10:49:40 +0600

    VERCEL.JSON ADDED

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const userHandler = require("./routeHandler/userHandler");
 const connectDB = require("./DB/connectDB");
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -15,22 +17,26 @@ app.use(
     })
 );
 
-// Connect to the database
-connectDB();
-
 // Routes
 app.use("/user", userHandler);
 app.use("/todo", todoHandler);
 
-// Error handler
+// Error Handler
 const errorHandler = (err, req, res, next) => {
     if (res.headersSent) {
         return next(err);
     }
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
 };
 app.use(errorHandler);
 
+// Root Route
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+// MongoDB Connection
+connectDB();
+
+// Export the app
+module.exports = app;
